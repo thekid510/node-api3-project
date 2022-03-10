1a1c025333c8c0b59e7c725bbdd29a4960ad08c9
thekid510 <76237067+thekid510@users.noreply.github.com>
2022-03-09 23:17:23 -0800

    not working as expected will debug later

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,48 +1,48 @@
 const User = require('../users/users-model')
 
 function logger(req, res, next) {
-  // DO YOUR MAGIC
   const timeStamp = new Date().toLocaleString()
   const method = req.method
   const url = req.originalUrl
   console.log(`[${timeStamp}] ${method} to ${url}`)
   next()
 }
-
 async function validateUserId(req, res, next) {
+  // DO YOUR MAGIC
   try{
     const user = await User.getById(req.params.id)
     if(!user){
-      next({ status: 404, message: 'user not found'})
+      next({ status: 400, message: 'user not found'})
     }else{
       req.user = user
       next()
     }
   }catch (err){
     res.status(500).json({
-      message: ' problem finding user',
+      message: 'problem finding user',
     })
   }
 }
 
 function validateUser(req, res, next) {
+  // DO YOUR MAGIC
   try{
     const user = await User.getById(req.params.id)
     if(!user){
-      next({ status: 404, message: 'user not found'})
+      next({ status: 400, message: 'missing required text field'})
     }else{
       req.user = user
       next()
     }
   }catch (err){
     res.status(500).json({
-      message: ' problem finding user',
+      message: 'problem finding user',
     })
   }
-
 }
 
 function validatePost(req, res, next) {
+  // DO YOUR MAGIC
   const { text } = req.body
   if(!text || !text.trim()){
     res.status(400).json({
